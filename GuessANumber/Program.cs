522c0871ee1a7b01aae3494ed2913f3b6331d263
Lyamson <vlyamson@gmail.com>
2024-04-06 15:24:56 +0300

    Initial commit

---
+++ GuessANumber/Program.cs
@@ -0,0 +1,17 @@
+﻿Random rnd = new Random();
+int number = rnd.Next(0, 101);
+
+int guess = -1;
+do{
+    do {
+        Console.Write("Your guess: ");
+    } while (!int.TryParse(Console.ReadLine(), out guess));
+    if (number > guess) {
+        Console.WriteLine("Higher");
+    }
+    else if(number < guess){
+        Console.WriteLine("Lower");
+    }
+} while (guess != number);
+
+Console.WriteLine("Congrats!");
